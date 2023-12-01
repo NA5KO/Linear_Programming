7b5f813bcbcb368c75e79c50a9fc87eb211b36ac
Amine Yahya <amine101yahya@gmail.com>
2023-12-01 22:43:24 +0100

    main page

--- Main.py
+++ Main.py
@@ -1,55 +1,148 @@
-import tkinter as tk
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget 
+from PyQt5.QtGui import QPixmap, QFont , QPalette, QBrush 
+from PyQt5.QtCore import Qt
+from PyQt5 import QtGui
 
 def exit_program():
-    root.destroy()
+    app.quit()
 
-root = tk.Tk()
-root.title("Project Name")
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-# Set window size and position
-window_width = 800
-window_height = 600
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x_coordinate = (screen_width / 2) - (window_width / 2)
-y_coordinate = (screen_height / 2) - (window_height / 2)
-root.geometry('%dx%d+%d+%d' % (window_width, window_height, x_coordinate, y_coordinate))
+        self.setWindowIcon(QtGui.QIcon('download.png'))
+        self.setWindowTitle("Python LP Exercises With Gurobi")
 
-# Background Image
+        # Set window size and position
+        window_width = 800
+        window_height = 600
+        screen_geometry = app.primaryScreen().geometry()
+        x_coordinate = int((screen_geometry.width() / 2) - (window_width / 2))
+        y_coordinate = int((screen_geometry.height() / 2) - (window_height / 2))
+        self.setGeometry(x_coordinate, y_coordinate, window_width, window_height)
 
-bg_image = tk.PhotoImage(file="s.jpg")
-background_label = tk.Label(root, image=bg_image)
-background_label.place(relwidth=1, relheight=1)
+        # Apply background image to window using stylesheet
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setBrush(QPalette.Window, QBrush(QPixmap("tech-background-1l29np90hcci8axg.jpg").scaled(self.size())))
+        self.setPalette(palette)
 
-# Project Name in the middle at the top
-project_name = tk.Label(root, text="LP Exercices", font=("Arial", 30))
-project_name.pack(pady=20)
+        # Project Name in the middle at the top
+        self.project_name = QLabel("Linear Programming Exercises", self)
+        self.project_name.setStyleSheet("color: #FF4949;")
+        self.project_name.setFont(QFont("Arial", 30, QFont.Bold))
+        self.project_name.setAlignment(Qt.AlignCenter)
 
-# Frame to contain exercise buttons
-exercise_frame = tk.Frame(root)
-exercise_frame.pack(pady=20)
+        # Description of the project
+        self.project_description = QLabel("This project aims to Let the user choose a Linear Programing Exercice.\n After choosing an exercice,the user can read the wording and then fill the fields\n with some variables which he can later solve the exercice based on those variables ", self)
+        self.project_description.setFont(QFont("Arial", 15))
+        self.project_description.setStyleSheet("color: white;")
+        self.project_description.setAlignment(Qt.AlignCenter)
 
-# Function to handle exercise selection
-def select_exercise():
-    # Implement the logic to handle exercise selection here
-    pass
+        # Layout to contain exercise buttons
+        self.exercise_layout = QVBoxLayout()
+        self.exercise_frame = QWidget(self)
+        self.exercise_frame.setLayout(self.exercise_layout)
+        self.exercise_layout.setAlignment(Qt.AlignHCenter)
 
-# Example buttons for exercises in three rows
-for i in range(1, 10):
-    exercise_button = tk.Button(exercise_frame, text=f"Exercise {i}", command=select_exercise, width=10, height=1, font=("Arial", 15), bg="white", fg="black", activebackground="black", activeforeground="white")
-    exercise_button.grid(row=(i-1)//3, column=(i-1)%3, padx=10, pady=10)
+        # Function to handle exercise selection
+        def select_exercise():
+            case = exercise_button.text()
+            if case == "Optimise Argiculture Zone":
+                from Exercice1 import Exercice1
+                self.exercice1 = Exercice1()
+                self.exercice1.show()
+            elif case == "Manage production of a company":
+                from Exercice2 import Exercice2
+                self.exercice2 = Exercice2()
+                self.exercice2.show()
+            elif case == "Planify the needs of human ressources":
+                from Exercice3 import Exercice3
+                self.exercice3 = Exercice3()
+                self.exercice3.show()
+            elif case == "choice of location of bank branches":
+                from Exercice4 import Exercice4
+                self.exercice4 = Exercice4()
+                self.exercice4.show()
+            elif case == "Positioning Problem":
+                from Exercice5 import Exercice5
+                self.exercice5 = Exercice5()
+                self.exercice5.show()
+            elif case == "Netwrok Flow Problem":
+                from Exercice6 import Exercice6
+                self.exercice6 = Exercice6()
+                self.exercice6.show()
+            elif case == "Exercise 7":
+                from Exercice7 import Exercice7
+                self.exercice7 = Exercice7()
+                self.exercice7.show()
+            elif case == "Exercise 8":
+                from Exercice8 import Exercice8
+                self.exercice8 = Exercice8()
+                self.exercice8.show()
+            elif case == "Exercise 9":
+                from Exercice9 import Exercice9
+                self.exercice9 = Exercice9()
+                self.exercice9.show()
+            else:
+                pass
 
-# Exit Button
-exit_button = tk.Button(root, text="Exit", command=exit_program, width=10, height=1, font=("Arial", 20), bg="white", fg="black", activebackground="black", activeforeground="white")
-exit_button.pack(pady=20)
+        # Example buttons for exercises
+        Exercise_names = [
+    "Optimise Argiculture Zone",
+    "Manage production of a company",
+    "Planify the needs of human ressources",
+    "choice of location of bank branches",
+    "Positioning Problem",
+    "Netwrok Flow Problem",
+    "Exercise 7",
+    "Exercise 8",
+    "Exercise 9"
+    ]
+        for exercise_name in Exercise_names:
+            exercise_button = QPushButton(exercise_name, self)
+            exercise_button.setFont(QFont("Arial", 12))
+            exercise_button.setStyleSheet("background-color: white; color: black; border: 1px solid white ; border-radius: 10px ; active {background-color: #FF4949;}")
+            exercise_button.setFixedSize(600, 40)
+            exercise_button.clicked.connect(select_exercise)
+            self.exercise_layout.addWidget(exercise_button)
 
-# Status Bar
-status_bar = tk.Label(root, text="Status: Ready", bd=1, relief=tk.SUNKEN, anchor=tk.W)
-status_bar.pack(side=tk.BOTTOM, fill=tk.X)
+        # Arrange buttons in 3 rows at the center
+        self.exercise_layout.addStretch()
+        self.exercise_layout.addStretch()
+        self.exercise_layout.addStretch()
 
-# Collaborators at the bottom corner
-collaborators = tk.Label(root, text="Collaborators:  A, B, C, D, E" , bd=1, relief=tk.SUNKEN, anchor=tk.W)
-collaborators.pack(side=tk.BOTTOM, anchor=tk.SE, pady=10)
+        # Exit Button
+        self.exit_button = QPushButton("Exit", self)
+        self.exit_button.setFont(QFont("Arial", 16))
+        self.exit_button.setFixedSize(100, 40)
+        self.exit_button.setStyleSheet("background-color: #FF4949; color: white; border: 1px solid white ; border-radius: 10px; active {background-color: #FF4949;}")
+        self.exit_button.clicked.connect(exit_program)
 
-# Packing elements to display
-root.mainloop()
+        exit_layout = QVBoxLayout()  # Create a layout for the exit button
+        exit_layout.addWidget(self.exit_button)
+        exit_layout.setAlignment(Qt.AlignCenter)  
+
+        # Collaborators listed under each other
+        self.collaborators = QLabel("Collaborators:\nAmine Yahya\nAmen Dhouibi\nInes Zghal\nLamya Bel Haj Sghayer\nRoua Timoumi", self)
+        self.collaborators.setFont(QFont("Arial", 12))
+        self.collaborators.setStyleSheet("color: White;")
+        self.collaborators.setAlignment(Qt.AlignCenter)  # Center-align the collaborators
+
+
+        # Arrange widgets
+        central_widget = QWidget()
+        layout = QVBoxLayout(central_widget)
+        layout.addWidget(self.project_name)
+        layout.addWidget(self.project_description)
+        layout.addWidget(self.exercise_frame)
+        layout.addLayout(exit_layout)
+        layout.addWidget(self.collaborators)
+        self.setCentralWidget(central_widget)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
